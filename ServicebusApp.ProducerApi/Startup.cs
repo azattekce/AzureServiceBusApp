a761a91c06e16c7c8d634357da059b54c3e679fe
azattekce <azattekce@gmail.com>
2022-05-21 15:23:54 +0300

    test

--- ServicebusApp.ProducerApi/Startup.cs
+++ ServicebusApp.ProducerApi/Startup.cs
@@ -22,6 +22,7 @@ namespace ServicebusApp.ProducerApi
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            //
         }
 
         public IConfiguration Configuration { get; }
